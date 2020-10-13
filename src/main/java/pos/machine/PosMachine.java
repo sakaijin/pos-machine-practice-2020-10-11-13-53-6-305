6b6a6b085e5c3783d7653240dc00054680266733
Erik <erik.daniel.nobleta@oocl.com>
2020-10-13 21:01:08 +0800

    (Creation) (Erik-1) - Pos v2 creation

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,37 @@
 package pos.machine;
 
-import java.util.List;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class PosMachine {
+
+    private String receipt = "***<store earning no money>Receipt***" + "\r\n";
     public String printReceipt(List<String> barcodes) {
-        return null;
+        int totalPrice = 0;
+        List<String> barcodeSet = barcodes.stream().distinct().collect(Collectors.toList());
+        for(String barcode: barcodeSet){
+            int timesCalled = timesCalled(barcodes, barcode);
+            List<ItemInfo> name = getItem(barcode);
+            totalPrice += calculateTotalPrices(timesCalled, name.get(0).getPrice());
+            receipt += "Name: " + name.get(0).getName() + ", " + "Quantity: " + timesCalled + ", " + "Unit price: " +
+                    name.get(0).getPrice() + " (yuan), " + "Subtotal: " + timesCalled*name.get(0).getPrice() + " (yuan)"
+                    + "\r\n";
+        }
+        return receipt + "----------------------" + "\r\n" + "Total: " + totalPrice + " (yuan)" +
+                "\r\n" + "**********************";
+    }
+
+    private int calculateTotalPrices(int timesCalled, int unitPrices) {
+        return timesCalled * unitPrices;
+    }
+
+    private int timesCalled(List<String> barcodes, String barcode) {
+        return Collections.frequency(barcodes, barcode);
+    }
+
+    private List<ItemInfo> getItem(String barcode) {
+        Predicate<ItemInfo> byBarcode = x -> x.getBarcode().equals(barcode);
+        return ItemDataLoader.loadAllItemInfos().stream().filter(byBarcode).collect(Collectors.toList());
     }
 }
